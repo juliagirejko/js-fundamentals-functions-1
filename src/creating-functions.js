1231a4e30f0adfc776398406ec7ba4a338004c4c
Julia Girejko <julia.girejko@talan.com>
2023-07-19 12:59:21 +0200

    funcions 1 commit

--- src/creating-functions.js
+++ src/creating-functions.js
@@ -7,6 +7,9 @@
 // 2      | 3
 //
 // TODO: write code below
+function incrementsBy1(number) {
+  return number + 1
+}
 
 // 2. Define a function that capitalises any string
 
@@ -19,6 +22,10 @@
 
 // TODO: write code below
 
+function toUpperString(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1)
+}
+
 // 3. Define a function that takes any person's name and returns it with a smiley :)!
 // Remember to make the name capitalized!
 //
@@ -29,6 +36,10 @@
 // Aiyana | Hi, Aiyana :)
 //
 // TODO: write code below
+
+function saySmile(name) {
+  return 'Hi, ' + name.charAt(0).toUpperCase() + name.slice(1) + ' :)'
+}
 
 // 4. Define a function that takes an array of data and returns how many strings are in the array.
 //
@@ -41,6 +52,15 @@
 // [1, 'edward']        | 1
 //
 // TODO: write code below
+function countString(arr) {
+  let arrSize = 0
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === 'string' || arr[i] instanceof String) {
+      arrSize += 1
+    }
+  }
+  return arrSize
+}
 
 // 5. Define a function that takes an object and adds a property 'edward' to the object if it doesn't yet exist with a default value of 'amazing'
 //
@@ -52,12 +72,18 @@
 // { edward: 'fabulous'}    | { edward: 'fabulous'}
 //
 // TODO: write code below
+function edwardIsAmazing(obj) {
+  if (!obj.edward) {
+    obj.edward = 'amazing'
+  }
+  return obj
+}
 
 // TODO: change each undefined below to be the name of the functions you defined
 module.exports = {
-  a: undefined, // 1. change undefined to be the name of the function you defined for the first TODO
-  b: undefined, // 2. change undefined to be the name of the function you defined for the second TODO)
-  c: undefined, // etc
-  d: undefined,
-  e: undefined
+  a: incrementsBy1, // 1. change undefined to be the name of the function you defined for the first TODO
+  b: toUpperString, // 2. change undefined to be the name of the function you defined for the second TODO)
+  c: saySmile, // etc
+  d: countString,
+  e: edwardIsAmazing
 }
